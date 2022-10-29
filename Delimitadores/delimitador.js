90d0db232693f13e633aa15d6585f4a169414b24
diegocantelli <diegocantelli19@gmail.com>
2022-10-29 19:09:09 -0300

    Exemplo com delimitadores

---
+++ Delimitadores/delimitador.js
@@ -0,0 +1,6 @@
+// ^ -> ira buscar no comeco com determinada expressao
+// $ -> ira buscar tudo que termina com determinada expressao
+
+const text = "Sucesso: você passou."
+
+console.log(text.match(/^Sucesso: você passou\.?$/g))
